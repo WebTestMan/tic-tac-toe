aed10a5aa9666b663d25124636948e815c37be5f
Mark Guhathakurta-Halliday <133343189+WebTestMan@users.noreply.github.com>
2025-04-11 11:10:04 +0100

    added player object

--- tictacttoe.js
+++ tictacttoe.js
@@ -3,6 +3,21 @@
 // 3x3 array
 // render game board
 
+// 1 | 2 | 3
+// 4 | 5 | 6
+// 7 | 8 | 9
+
+const winningConditions = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9],
+    [3, 5, 7]
+]
+
 const gameBoard = (function () {
     const nought = 'O'
     const cross = 'X'
@@ -14,33 +29,61 @@ const gameBoard = (function () {
         }
         console.log('');
     }
-    const updateGameBoard = (xPosition, yPosition, noughtOrCross) => {
-        gameBoardArray[xPosition][yPosition] = noughtOrCross;
+    const updateGameBoard = (position, noughtOrCross) => {
+        gameBoardArray[position[0]][position[1]] = noughtOrCross;
         renderGame();
     }
 
-    const gameTurn = (xPosition, yPosition, noughtOrCross) => {
-        if (gameBoardArray[xPosition][yPosition] === nought || gameBoardArray[xPosition][yPosition] === cross) {
+    const gameTurn = (position, noughtOrCross) => {
+        if (gameBoardArray[position[0]][position[1]] === nought || gameBoardArray[position[0]][position[1]] === cross) {
             console.log("Space already taken try again...")
         } else {
-            updateGameBoard(xPosition, yPosition, noughtOrCross);
+            updateGameBoard(position, noughtOrCross);
             checkWinCondition();
         }
     }
 
     const checkWinCondition = () => {
+        const xPosition = gameBoardArray.filter((position) => {
+            return position === 'X'
+        })
+        const oPosition = gameBoardArray.filter((position) => {
+            return position === 'O'
+        })
+        xPosition.filter((result) => {
+            return winningConditions
+        })
 
+        oPosition.filter((result) => {
+            return winningConditions
+        })
+
+        /* 
+        check if the gameboard matches any of the results array
+        if gameboardarray 
+        */
 
         console.log('YOU WIN!!')
     }
     return { gameTurn };
 })();
 
-gameBoard.gameTurn(0, 0, 'X');
-gameBoard.gameTurn(1, 0, 'O');
-gameBoard.gameTurn(1, 0, 'X');
+function player() {
+    getPlayerName = () => { return playerName };
+    setPlayerName = (playerName) => { this.playerName = playerName };
+    getPlayerPiece = () => { return playerPiece };
+    setPlayerPiece = (playerPiece) => { this.playerPiece = playerPiece };
 
+    return { getPlayerName, setPlayerName, getPlayerPiece, setPlayerPiece };
+}
 
+gameBoard.gameTurn([0, 1], 'X');
+
+player1 = player();
+player1.setPlayerName('Steve');
+player1.setPlayerPiece('X');
+console.log(player1.getPlayerName());
+console.log(player1.getPlayerPiece());
 
 
 
