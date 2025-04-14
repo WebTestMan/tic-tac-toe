cdb90f961a69f6152abbe9b5865625d19ee4dea0
Mark Guhathakurta-Halliday <133343189+WebTestMan@users.noreply.github.com>
2025-04-14 16:46:27 +0100

    added detail and style to page

--- tictacttoe.js
+++ tictacttoe.js
@@ -5,7 +5,6 @@
 const nought = 'O'
 const cross = 'X'
 
-
 const gameBoard = (function () {
     const gameBoardArray = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']];
     renderGame = () => {
@@ -13,6 +12,15 @@ const gameBoard = (function () {
             console.log(`${gameBoardArray[i][0]} | ${gameBoardArray[i][1]} | ${gameBoardArray[i][2]}`);
         }
         console.log('');
+
+        const gameDiv = document.querySelector('.game-display');
+
+        for (i = 0; i < 9; i++) {
+            const gameSquare = document.createElement('div');
+            gameSquare.setAttribute('class', 'game-square');
+            gameSquare.setAttribute('id', 'id-' + i);
+            gameDiv.appendChild(gameSquare);
+        }
     }
     updateGameBoard = (position, noughtOrCross) => {
         gameBoardArray[position[0]][position[1]] = noughtOrCross;
@@ -31,7 +39,7 @@ const gameBoard = (function () {
         return gameBoardArray;
     }
 
-    return { updateGameBoard, getGameBoard, resetGameBoard }
+    return { updateGameBoard, getGameBoard, resetGameBoard, renderGame }
 })();
 
 function player() {
@@ -84,16 +92,16 @@ Player Score: ${playerScore}`
 }
 
 const gameController = (function () {
-    const winningConditions = [
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9],
-        [1, 4, 7],
-        [2, 5, 8],
-        [3, 6, 9],
-        [1, 5, 9],
-        [3, 5, 7]
-    ]
+    // const winningConditions = [
+    //     [1, 2, 3],
+    //     [4, 5, 6],
+    //     [7, 8, 9],
+    //     [1, 4, 7],
+    //     [2, 5, 8],
+    //     [3, 6, 9],
+    //     [1, 5, 9],
+    //     [3, 5, 7]
+    // ]
     gameBoardArray = gameBoard.getGameBoard();
 
     const gameTurn = (position, currentPlayer) => {
@@ -147,26 +155,6 @@ const gameController = (function () {
     return { gameTurn, checkWinCondition };
 })();
 
-player1 = player();
-player1.setUpPlayer('Steve', 'X');
-console.log('Player 1 ' + player1.displayPlayerDetails());
-
-aiPlayer = player();
-aiPlayer.setUpOpponent(player1);
-console.log('AI ' + aiPlayer.displayPlayerDetails());
-
-const aiPlayerName = aiPlayer.getPlayerName();
-const player1Name = player1.getPlayerName();
-console.log(`The players are Human: ${player1Name} & AI: ${aiPlayerName}`);
-
-
-// gameController.gameTurn([1, 1], aiPlayer);
-gameController.gameTurn([1, 0], aiPlayer);
-
-gameController.gameTurn([0, 0], player1);
-gameController.gameTurn([1, 1], player1);
-gameController.gameTurn([2, 2], player1);
-
-gameController.gameTurn([1, 2], aiPlayer);
+gameBoard.renderGame();
 
 // checks for win scenario
