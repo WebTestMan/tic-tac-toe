7431aead8335358b093212b8dc9ef25e1b151133
Mark Guhathakurta-Halliday <133343189+WebTestMan@users.noreply.github.com>
2025-04-16 15:55:43 +0100

    Disabled some functionality, basic game working with web interface

--- tictacttoe.js
+++ tictacttoe.js
@@ -4,31 +4,48 @@
 // 7 | 8 | 9
 const nought = 'O'
 const cross = 'X'
+let currentPlayer = nought;
+let nextPlayer = cross;
 
+// buttons is a node list. It looks and acts much like an array.
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+    // and for each one we add a 'click' listener
+    button.addEventListener("click", () => {
+        button.classList.add('class', 'button-clicked');
+    });
+});
+
+
+
+
+// Game Board Module
 const gameBoard = (function () {
     const gameBoardArray = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']];
     renderGame = () => {
-        for (let i = 0; i < gameBoardArray.length; i++) {
-            console.log(`${gameBoardArray[i][0]} | ${gameBoardArray[i][1]} | ${gameBoardArray[i][2]}`);
-        }
-        console.log('');
 
         const gameDiv = document.querySelector('.game-display');
-
+        gameDiv.innerHTML = '';
         for (let i = 0; i < gameBoardArray.length; i++) {
             for (let j = 0; j < gameBoardArray.length; j++) {
                 const gameSquare = document.createElement('div');
                 gameSquare.setAttribute('class', 'game-square');
                 gameSquare.setAttribute('id', `id-${i}-${j}`);
+                gameSquare.addEventListener('click', () => {
+                    console.log(`${currentPlayer} id-${i}-${j}`)
+                    gameController.gameTurn([i, j], currentPlayer);
+                });
                 gameDiv.appendChild(gameSquare);
             }
         }
+
     }
 
     updateGameBoard = (position, noughtOrCross) => {
         gameBoardArray[position[0]][position[1]] = noughtOrCross;
-        const gameSquare = document.querySelector(`#id-${position[0]}-${position[0]}`,)
-        gameSquare.innerHTML = noughtOrCross;
+        console.log(`id-${position[0]}-${position[1]} Piece: ${noughtOrCross}`);
+        const gameSquare = document.getElementById(`id-${position[0]}-${position[1]}`);
+        gameSquare.textContent = noughtOrCross;
     }
 
 
@@ -39,6 +56,7 @@ const gameBoard = (function () {
             }
         }
         renderGame();
+        gameController.updateGameStatus('');
     }
     getGameBoard = () => {
         return gameBoardArray;
@@ -47,7 +65,16 @@ const gameBoard = (function () {
     return { updateGameBoard, getGameBoard, resetGameBoard, renderGame }
 })();
 
+
+
+
+/// Player Factory Function
 function player() {
+
+    let playerObject = {
+        playerName: 'Steve',
+        playerPiece: 'X',
+    }
 
     let playerName = '';
     let playerPiece = '';
@@ -96,39 +123,65 @@ Player Score: ${playerScore}`
     return { getPlayerName, getPlayerPiece, displayPlayerDetails, setUpPlayer, setUpOpponent, increasePlayerScore, getPlayerScore, setPlayerScore };
 }
 
+// Game Controller module
 const gameController = (function () {
 
     gameBoardArray = gameBoard.getGameBoard();
 
-    const startGame = () => {
+    const startGame = (player1Info, player2info = false) => {
+        let playerObject = {
+            playerName: 'Steve',
+            playerPiece: 'X',
+        }
+        if (player1Info) {
+            playerObject.playerName = player1Info.playerName;
+            playerObject.playerPiece = player1Info.playerPiece = player1Info.piece;
+        }
+
         player1 = player();
-        player1.setUpPlayer('Steve', cross);
-        console.log(player1.displayPlayerDetails());
+        player1.setUpPlayer(playerObject.playerName, playerObject.playerPiece);
 
         player2 = player();
-        player2.setUpOpponent(player1);
-
+        if (!player2info) {
+            player2.setUpOpponent(player1);
+        } else {
+            player2.setUpPlayer(player2info.playerName, player2info.playerPiece);
+        }
         gameBoard.renderGame();
     }
 
-    const gameTurn = (position, currentPlayer) => {
-        const noughtOrCross = currentPlayer.getPlayerPiece();
-        if (gameBoardArray[position[0]][position[1]] === nought || gameBoardArray[position[0]][position[1]] === cross) {
-            console.log("Space already taken try again...")
-        } else {
-            gameBoard.updateGameBoard(position, noughtOrCross)
-            checkWinCondition(currentPlayer);
+    const gameTurn = (position, noughtOrCross) => {
+        if (currentPlayer === cross) {
+            currentPlayer = nought;
+            console.log('player changed to O');
+        } else if (currentPlayer === nought) {
+            currentPlayer = cross;
+            console.log('player changed to X');
+        }
+
+        updateGameStatus(`${currentPlayer}'s turn...`);
+        if (checkTakenPosition(position) === false) {
+            gameBoard.updateGameBoard(position, noughtOrCross);
+            checkWinCondition(noughtOrCross);
         }
     }
 
-    const checkWinCondition = (currentPlayer) => {
-        let oPosition = [];
-        let xPosition = [];
+    const checkTakenPosition = (position) => {
+        let positionTaken;
+        if (gameBoardArray[position[0]][position[1]] === nought || gameBoardArray[position[0]][position[1]] === cross) {
+            updateGameStatus("Space already taken try again...");
+            positionTaken = true;
+        } else {
+            positionTaken = false;
+        }
+        return positionTaken;
+    }
+
+    const checkWinCondition = (currentPiece) => {
 
         const row = 3;
         const col = 3;
-        const currentPiece = currentPlayer.getPlayerPiece();
-        const currentPlayerName = currentPlayer.getPlayerName()
+        let winStatus = false;
 
         for (let i = 0; i < col; i++) {
             for (let j = 0; j < col; j++) {
@@ -136,8 +189,7 @@ const gameController = (function () {
                     break;
                 }
                 if (j === col - 1) {
-                    console.log(`${currentPlayerName} is the winner!`);
-                    return currentPiece; s
+                    winStatus = true;
                 }
             }
 
@@ -146,27 +198,51 @@ const gameController = (function () {
                     break;
                 }
                 if (j === row - 1) {
-                    console.log(`${currentPlayerName} is the winner!`);
-                    return currentPiece;
+                    winStatus = true;
                 }
             }
         }
         if ((gameBoardArray[0][0] === currentPiece && gameBoardArray[1][1] === currentPiece && gameBoardArray[2][2] === currentPiece)
             || (gameBoardArray[0][2] === currentPiece && gameBoardArray[1][1] === currentPiece && gameBoardArray[2][0] === currentPiece)) {
-            console.log(`${currentPlayerName} is the winner!`);
-            return currentPiece;
+            winStatus = true;
         }
 
+        if (winStatus === true) {
+            const winMessage = `${currentPiece} is the winner!`;
+            console.log(winMessage);
+            updateGameStatus(winMessage);
+            return winMessage;
+        }
+        checkGameOver();
     }
 
-    return { startGame, gameTurn, checkWinCondition };
+    const checkGameOver = () => {
+        console.log('Checking for game over')
+        let fullGameBoard = 0;
+        for (i = 0; i < gameBoardArray.length; i++) {
+            for (j = 0; j < gameBoardArray.length; j++) {
+                if (gameBoardArray[i][j] === nought ||
+                    gameBoardArray[i][j] === cross
+                ) {
+                    fullGameBoard++;
+                }
+            }
+
+        }
+        console.log(fullGameBoard);
+        if (fullGameBoard >= 9) {
+            updateGameStatus('GAME OVER - DRAW!');
+        }
+    }
+
+    const updateGameStatus = (message) => {
+        const gameStatus = document.querySelector('#game-status');
+        gameStatus.textContent = (message);
+    }
+
+    return { startGame, gameTurn, checkWinCondition, updateGameStatus };
 })();
 
-
-gameController.startGame();
-gameController.gameTurn([0, 1], player1);
-gameController.gameTurn([1, 1], player2);
-gameController.gameTurn([2, 1], player1);
-gameController.gameTurn([1, 2], player2);
-
-// checks for win scenario
+gameBoard.renderGame();
+const resetBtn = document.getElementById('reset-btn');
+resetBtn.addEventListener('click', gameBoard.resetGameBoard);
